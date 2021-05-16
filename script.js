a7b7a570aef445eae5c5a6c8e758e17b86ff385b
Justin775 <jaburto775@gmail.com>
2021-05-15 21:56:52 -0500

    comments added to js file with some organizing

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 var generateBtn = document.querySelector("#generate");
-
+// Variables Charachters for password 
 var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
  "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X","Y", "Z"]
 var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
  "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
  var numChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
  var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "<", ">","/", "~", "=", "+", "-"]
-
 
 function writePassword() {
   var password = genPassword();
@@ -18,18 +17,19 @@ function writePassword() {
 
 generateBtn.addEventListener("click", writePassword);
 
+// Checks arrays length and randomly selects 
 function random(arr) {
   let randomindex = Math.floor(Math.random()*arr.length)
   return arr[randomindex]
 } 
 
-function passwordOptions() {
+function passwordOps() {
   let passwordLength = parseInt(prompt ("How many characters do you want your password? (between 8 and 128 characters)"));
   if (passwordLength < 8 || passwordLength > 128) {
     alert ("Passworf length must be between 8 and 128.")
-    return passwordOptions()
+    return passwordOps()
   }
-    
+// Prompts
  let uppers = confirm("Do you need to include Uppercase Letters?")
  let lowers = confirm("Do you need to include Lowercase Letters?")
  let numbers = confirm("Do you need to include Numbers?")
@@ -42,38 +42,41 @@ function passwordOptions() {
 }
 
 function genPassword() {
-  let choices = passwordOptions();
-  let concats = []
+  let choices = passwordOps();
+  let change = []
   let guaranted = []
-  let finalpassword = []
+  let finalPW = []
+
+  //if loops upn selecting choices
 if (choices.specchar) {
-  concats =[...concats, ...specialChar]
+  change =[...change, ...specialChar]
   guaranted.push(random(specialChar))
   }
 
-if (choices.numbers) {
-  concats = concats.concat(numChar)
-  guaranted.push(random(numChar))
-}
-
 if (choices.lowers) {
-  concats = concats.concat(lowerChar)
+  change = change.concat(lowerChar)
   guaranted.push(random(lowerChar))
 }
 
+if (choices.numbers) {
+  change = change.concat(numChar)
+  guaranted.push(random(numChar))
+}
+
 if (choices.uppers) {
-  concats = concats.concat(upperChar)
+  change = change.concat(upperChar)
   guaranted.push(random(upperChar))
 }
-console.log(concats)
+
+console.log(change)
 console.log(guaranted)
 
 
 for(let i = 0; i < choices.passwordLength; i++) {
-finalpassword.push(random(concats))
+finalPW.push(random(change))
 }
 for(let i = 0; i < guaranted.length; i++) {
-  finalpassword[i] = guaranted[i]
+  finalPW[i] = guaranted[i]
   }
-return finalpassword.join("")
+return finalPW.join("")
 }
